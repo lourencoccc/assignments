22caa22a5660abdf9e67053395fac41cd2ab15ff
Joao Lourenco Souza Jr <lourenco@laptop.lourencoccc>
2018-04-18 08:23:54 -0300

    add folder with pratice code from linear algebra refresher

---
+++ linearalgebra/vector.py
@@ -0,0 +1,27 @@
+
+class Vector(object):
+
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinate must to be nonempty')
+        except TypeError:
+            raise TypeError('The coordinantes must be an iterable')
+
+    def __str__(self):
+            return "Vector: {}".format(self.coordinates)
+
+    def __eq__(self, v):
+            return self.coordinates == v.coordinates
+
+my_vector = Vector([1,2,3])
+print(my_vector)
+my_vector2 = Vector([1,2,3])
+my_vector3 = Vector([-1,2,3])
+print(my_vector == my_vector2)
+print(my_vector == my_vector3)
